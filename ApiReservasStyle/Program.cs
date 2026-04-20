5b9ba951842d4c59e23773c049fe0fbd2e496dde
Dulce Labra <dulce.labra@repobox.com.mx>
2026-04-19 18:18:14 -0700

    Token

--- ApiReservasStyle/Program.cs
+++ ApiReservasStyle/Program.cs
@@ -25,6 +25,11 @@ builder.Services.AddControllers()
      });
 
 // Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+//JWT
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
@@ -60,33 +65,6 @@ builder.Services.AddAuthentication(options =>
         }
     };
 });
-
-
-//JWT
-//builder.Services.AddAuthentication(options =>
-//{
-//    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-//    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-//})
-//.AddJwtBearer(options =>
-//{
-//    options.TokenValidationParameters = new TokenValidationParameters
-//    {
-//        ValidateIssuer = true,
-//        ValidateAudience = true,
-//        ValidateLifetime = true,
-//        ValidateIssuerSigningKey = true,
-
-//        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-//        ValidAudience = builder.Configuration["Jwt:Audience"],
-
-//        IssuerSigningKey = new SymmetricSecurityKey(
-//            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])
-//        ),
-
-//        ClockSkew = TimeSpan.Zero
-//    };
-//});
 
 // RATE LIMITING
 builder.Services.AddRateLimiter(options =>
